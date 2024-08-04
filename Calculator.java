edb79f2e4533fe4427fd478b9c4302eff323595e
imhaley <imhaley1536@gmail.com>
2024-08-04 10:34:00 +0800

    feat: new file

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+
+    private int x = 0;
+    private boolean= true;
+    private String = "tt";
+
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
